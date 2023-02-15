7a2c310c4781630e4f4c8942478ddfcd983e3fdb
Anup Upadhaya <au.anupupadhaya@gmail.com>
2023-02-15 21:22:24 +0545

    changes in routes

--- lib/presentation/routes/routes_generator.dart
+++ lib/presentation/routes/routes_generator.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pull_refresh/business_logic/bloc/homepage_bloc.dart';
+import 'package:pull_refresh/data/repo/homepage_repo.dart';
 import 'package:pull_refresh/presentation/screens/homepage.dart';
 
 class RouteGenerator {
+  final HomepageBloc _homepageBloc = HomepageBloc(HomePageRepo());
+
   Route<dynamic> generateRoute(RouteSettings settings) {
     final args = settings.arguments;
 
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(
-          builder: (_) => const MyHomePage(title: "API & BLOC"),
+          builder: (_) => BlocProvider<HomepageBloc>.value(
+            value: _homepageBloc,
+            child: const MyHomePage(title: "API & BLOC"),
+          ),
         );
 
       default:
